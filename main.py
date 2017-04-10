d837daaba1e8b2dba72356c1945868f1bccf2ea5
Hao Yuan <mctinker2010@gmail.com>
2017-04-10 16:21:55 -0700

    unregularized

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ flags.DEFINE_integer("updates_per_epoch", 1000, "number of updates per epoch")
 flags.DEFINE_integer("max_epoch", 200, "max epoch")
 flags.DEFINE_float("learning_rate", 1e-2, "learning rate")
 flags.DEFINE_string("working_directory", "/tempspace/hyuan/VAE", "the file directory")
-flags.DEFINE_integer("hidden_size", 1, "size of the hidden VAE unit")
+flags.DEFINE_integer("hidden_size", 3, "size of the hidden VAE unit")
 flags.DEFINE_integer("channel", 128, "size of initial channel in decoder")
 
 FLAGS = flags.FLAGS
