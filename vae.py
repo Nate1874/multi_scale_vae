ccba09c5be7b8ef9f9643819a878da20d15b46b4
Hao Yuan <mctinker2010@gmail.com>
2017-05-04 11:31:56 -0700

    32*3*3

--- vae.py
+++ vae.py
@@ -11,8 +11,8 @@ class VAE(Generator):
         self.working_directory = '/tempspace/hyuan/VAE'
         self.height = 64
         self.width = 64                           
-        self.modeldir = './modeldir_celeba_3_128_reshape'
-        self.logdir = './logdir_celeba_3_128_reshape'
+        self.modeldir = './modeldir_celeba_3_32_reshape'
+        self.logdir = './logdir_celeba_3_32_reshape'
         self.hidden_size = hidden_size
         self.batch_size = batch_size
         self.learning_rate =learning_rate
